b65e1d060f5542cdeee69dc2c823e964c2a0bb8f
Vishal@7831 <vishalpatel78317@gmial.com>
2024-09-10 20:47:25 +0530

    VP:responsive chnage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="./style.css" />
+      </head>
       <body className={inter.className}>
         <SpeedInsights />
         <Navbar />
